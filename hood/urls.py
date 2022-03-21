a6a88c173e00e71b2d6dab575681040a9da5ed88
GeorgeM101 <georgmboya@gmail.com>
2022-03-21 06:42:10 +0300

    created login view

--- hood/urls.py
+++ hood/urls.py
@@ -2,5 +2,7 @@ from django.urls.conf import path
 from . import views
 
 urlpatters = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('register',views.registration_form,name='registration_form'),
+
 ]
--- hood/views.py
+++ hood/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, logout as userlogout,login as userlogin
+from django.contrib import messages
+from django.contrib.auth.hashers import make_password
+
+
+
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html')
+
+def registration_form(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        if password == confirm_password: 
+            user = User(username=username, email=email, password=make_password(password))
+            user.save()
+            messages.add_message(request, messages.SUCCESS, "You have successfully registered!")
+            return redirect(login)
+    return render(request,'registration/registration_form.html',)
